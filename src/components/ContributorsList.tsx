d2c0f8a66e54e9976c506b4466716b5ef37a14b7
Josafat Jimenez <josa221001@gmail.com>
2024-10-06 00:28:54 -0600

    fix: url repo

--- src/components/ContributorsList.tsx
+++ src/components/ContributorsList.tsx
@@ -12,7 +12,7 @@ export const ContributorsList = () => {
     const fetchContributors = async () => {
       try {
         const response = await fetch(
-          "https://api.github.com/repos/appwrite/hacktoberfest/pulls?state=closed&?per_page=100"
+          "https://api.github.com/repos/Guayaba-Devs/Guayaba-Hacktober-Challenge/pulls?state=closed&?per_page=100"
         );
         const data = await response.json();
 
